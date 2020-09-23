a521f88e28f95dd8f1d4237d21ea240bfeba31cd
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-09-24 00:29:34 +0300

    Updates

---
+++ precache-manifest.13d5a5e5433a4b02b91826b0c6ba14cf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cad4462481bc4f9243b4bc0d3aa20f",
+    "url": "/HW19-ReactJS.Router/index.html"
+  },
+  {
+    "revision": "3a3a1ebabc95914879dd",
+    "url": "/HW19-ReactJS.Router/static/css/main.81c7e5d1.chunk.css"
+  },
+  {
+    "revision": "d53bd2da0d09473e41fb",
+    "url": "/HW19-ReactJS.Router/static/js/2.73934e1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW19-ReactJS.Router/static/js/2.73934e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3a1ebabc95914879dd",
+    "url": "/HW19-ReactJS.Router/static/js/main.8524ab22.chunk.js"
+  },
+  {
+    "revision": "876cad441e346f67a192",
+    "url": "/HW19-ReactJS.Router/static/js/runtime-main.6932c82d.js"
+  },
+  {
+    "revision": "0002e5f6fa980eed02d3897ecc5eb36d",
+    "url": "/HW19-ReactJS.Router/static/media/comment.0002e5f6.svg"
+  },
+  {
+    "revision": "39726703564a2a71e8077948ac5d964c",
+    "url": "/HW19-ReactJS.Router/static/media/confirm.39726703.svg"
+  },
+  {
+    "revision": "da3338122f87907c1fb994f17002f866",
+    "url": "/HW19-ReactJS.Router/static/media/down-arrow.da333812.svg"
+  },
+  {
+    "revision": "5c87994c964d115c484b909be298743b",
+    "url": "/HW19-ReactJS.Router/static/media/female.5c87994c.svg"
+  },
+  {
+    "revision": "12220a2259c8f1718847197c4bfdb84e",
+    "url": "/HW19-ReactJS.Router/static/media/like.12220a22.svg"
+  },
+  {
+    "revision": "2c4b5bdb5911cdf4bba1ee8694aea8c5",
+    "url": "/HW19-ReactJS.Router/static/media/main.2c4b5bdb.jpg"
+  },
+  {
+    "revision": "075829956d01b4f9d072ca02aa84e2f2",
+    "url": "/HW19-ReactJS.Router/static/media/male.07582995.svg"
+  },
+  {
+    "revision": "444db2463624b56ae55674c6e654d9b9",
+    "url": "/HW19-ReactJS.Router/static/media/share.444db246.svg"
+  },
+  {
+    "revision": "2508df2055ae3e150ddc505e107f95e7",
+    "url": "/HW19-ReactJS.Router/static/media/unknown.2508df20.svg"
+  },
+  {
+    "revision": "8f047106898ba92d2dc7e03f29f16bed",
+    "url": "/HW19-ReactJS.Router/static/media/upload.8f047106.svg"
+  },
+  {
+    "revision": "e7875f017f6ec13298bbe3b818bbbd96",
+    "url": "/HW19-ReactJS.Router/static/media/user.e7875f01.svg"
+  }
+]);
